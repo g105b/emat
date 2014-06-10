d1ef364fc4d8bc4b76ed0af9c457ec88c5444294
Greg Bowler <greg.bowler@g105b.com>
2014-06-10 22:03:57 +0100

    Save defaults, type check

--- Class/Settings.php
+++ Class/Settings.php
@@ -49,7 +49,7 @@ private $_conf = [
 	"DRYRUN" => [
 		"description" => "Dry run (yes/no)",
 		"type" => "bool",
-		"default" => true,
+		"default" => "yes",
 	],
 ];
 
@@ -66,23 +66,76 @@ public function __construct() {
  * Asks user for all required information, serialises to settings file.
  */
 private function create() {
+	// Loop over every conf variable.
 	foreach ($this->_conf as $code => $details) {
 		$prompt = $details["description"] . " ";
 		
+		// If there is a default value, display to user.
 		if(isset($details["default"])) {
 			$prompt .= "[" . $details["default"] . "] ";
 		}
 
 		$prompt .= ": ";
 
+		// Only progress when value is set (from user input or default).
 		while(!isset($details["value"])) {
 			$input = trim(readline($prompt));
 
+			// If user input is given and datatype is allowed, store value.
 			if(strlen($input) > 0) {
-				$details["value"] = $input;
+				if(isset($details["type"])) {
+					// Perform a type check on provided input.
+					switch($details["type"]) {
+					case "number":
+						if(is_numeric($input)) {
+							$details["value"] = (double)$input;
+						}
+						break;
+					case "int":
+					case "integer":
+						if(ctype_digit($input)) {
+							$details["value"] = (int)$input;
+						}
+						break;
+					case "bool":
+					case "boolean":
+						if(strtolower($input[0]) == "y") {
+							$details["value"] = true;
+						}
+						else if(strtolower($input[0]) == "n") {
+							$details["value"] = false;
+						}
+						break;
+					}
+
+					if(!isset($details["value"])) {
+						// Type check failed.
+						fwrite(
+							STDOUT, 
+							"Parameter must be type " 
+							. $details["type"]
+							. "." 
+							. PHP_EOL
+						);
+					}
+				}
+				else {
+					// No type check required (string input allowed).
+					$details["value"] = $input;					
+				}
 			}
 			else if(isset($details["default"])) {
-				$details["value"] = $details["default"];
+				// Convert default "yes"/"no" to true/false.
+				if(isset($details["type"])
+				&& ($details["type"] == "bool" 
+				|| $details["type"] == "boolean")
+				&& is_string($details["default"])) {
+					$details["value"] = ($details["default"] == "yes");
+				}
+				else {
+					$details["value"] = $details["default"];
+				}
+
 				fwrite(
 					STDOUT, 
 					"Using default value, " . $details["default"] . PHP_EOL
@@ -93,6 +146,13 @@ private function create() {
 			}
 		}
 	}
+
+	// All conf variables set - output to file.
+	$this->save();
+}
+
+private function save() {
+	// TODO.
 }
 
 }#
