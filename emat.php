ea4a8f4099837e27ff7fac1d138b991f0e387119
Greg Bowler <greg.bowler@g105b.com>
2014-06-10 18:11:08 +0100

    Code up main script

---
+++ emat.php
@@ -0,0 +1,25 @@
+<?php
+require "Class/Settings.php";
+require "Class/Data.php";
+require "Class/Trade.php";
+
+// Only proceed when a settings file can be loaded.
+while(!Settings::exists()) {
+	Settings::create();
+}
+
+// Load settings and optional historical data.
+$settings = Settings::load();
+$data = Data::load();
+
+// Loop until the Trade class returns false.
+// When trading on paper, Trade::next() will return false when the historical
+// data ends. When trading for real, false will only be returned from a fatal
+// error.
+while(false !== ($tradeAction = Trade::next()) {
+	fwrite(STDOUT, $tradeAction);
+}
+
+// Once the loop is broken, no more trading will take place until the script
+// is run again.
+fwrite(STDOUT, PHP_EOL . "emat terminated" . PHP_EOL);
